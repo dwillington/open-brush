c111333acf6f7bf53a2c872c99b2f5121ad13e59
dwillington <dwillington@yahoo.com>
2024-03-16 21:19:16 -0300

    Update ob.sh

--- bash/ob.sh
+++ bash/ob.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# to get this to work, you need to export OB_HOST=192.168.50.114 or whatever your IP address in quest device is (process is documented in the README).
+
 if [[ -z "${OB_HOST}" ]]; then
   host_name="localhost"
 else
   host_name=${OB_HOST}
 fi
-curl --get --data-urlencode "$1" -v http://$host_name:40074/api/v1 2>/dev/null
+curl --get --data-urlencode "$1"  http://$host_name:40074/api/v1
