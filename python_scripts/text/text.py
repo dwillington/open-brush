0812f3f115b02aa1f66dd7c37325c5f478e2a381
dwillington <dwillington@protonmail.com>
2024-04-11 17:44:56 -0300

    commit

--- python_scripts/text/text.py
+++ python_scripts/text/text.py
@@ -2,6 +2,8 @@ import os
 from ob import ob
 import argparse
 from xml.dom import minidom
+from svg.path import parse_path
+
 import urllib
 import re
 
@@ -78,6 +80,15 @@ def text():
 
   
   for g in glyphs:
+    d = path.getAttribute('d')
+    parsed = parse_path(d)
+    print('Objects:\n', parsed, '\n' + '-' * 20)
+    for obj in parsed:
+        print(type(obj).__name__, ', start/end coords:', ((round(obj.start.real, 3), round(obj.start.imag, 3)), (round(obj.end.real, 3), round(obj.end.imag, 3))))
+    print('-' * 20)
+    input("Press Enter to continue...")
+    continue
+
     print(g)
     ob.new()
     ob.brush.type("Paper")
