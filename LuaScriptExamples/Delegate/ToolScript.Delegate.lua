33ca6b11d37f6d13dc7a08e3fc8e3278b000abdc
dwillington <dwillington@protonmail.com>
2024-04-14 21:52:46 -0300

    commit

--- LuaScriptExamples/Delegate/ToolScript.Delegate.lua
+++ LuaScriptExamples/Delegate/ToolScript.Delegate.lua
@@ -27,8 +27,8 @@ end
 function handleRequest(result)
     print("in handleRequest: " .. result)
     -- result='{"svgPath": "M703 1129l306 139l61 -188l-328 -68v14q0 53 -39 103zM732 983l226 -248l-160 -116l-166 291q63 18 100 73zM601 910l-165 -291l-160 116l226 248q37 -55 99 -73zM492 1012l-328 68l61 188l305 -139q-38 -50 -38 -104v-13zM555 1147l-37 333h198l-37 -333q-35 13 -62 13t-62 -13z"}'
-    -- json_data = json:parse(result)
-    -- svgPath = json_data.svgPath
+    json_data = json:parse(result)
+    svgPath = json_data.svgPath
 
     svgPath = 'M703 1129l306 139l61 -188l-328 -68v14q0 53 -39 103zM732 983l226 -248l-160 -116l-166 291q63 18 100 73zM601 910l-165 -291l-160 116l226 248q37 -55 99 -73zM492 1012l-328 68l61 188l305 -139q-38 -50 -38 -104v-13zM555 1147l-37 333h198l-37 -333q-35 13 -62 13t-62 -13z'
 
